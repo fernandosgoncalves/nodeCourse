fef5c102f0f3f4d2c1d4e000477b705191296269
Fernando Silvano Gonçalves <fernandosilgon@gmail.com>
2024-05-06 22:11:40 -0300

    Aula 33

--- Aula27/primeiro-app/src/App.js
+++ Aula27/primeiro-app/src/App.js
@@ -1,45 +1,31 @@
-import { useState } from 'react';
-
-//Aula 30
-//import Nome from './components/Nome';
+import { useState, useEffect } from 'react';
 
 function App(){
-  //Aula 30
-  //const [aluno, setAluno] = useState("Programador")
-
-  //function handleChangeName(nome){
-  //  setAluno(nome);
-  //}
-
   const [input, setInput] = useState('');
   const [tarefas, setTarefas] = useState([
     'Pagar conta de luz',
     'Estudar React JS', 
     'Estudar Teclado' 
   ]);
-  //const [nome, setNome] = useState('');
-  //const [email, setEmail] = useState('');
-  //const [idade, setIdade] = useState(0);
 
-  /*const [user, setUser] = useState({
-    nome: 'Joaquim',
-    idade: '55',
-    email: 'fulano@fulano.com'
-  });*/
+  useEffect(() =>{
+    const tarefasStorage = localStorage.getItem('@tarefa');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+  }, []);
+
+  useEffect(()=> {
+     localStorage.setItem('@tarefa', JSON.stringify(tarefas))
+  }, [tarefas]);
 
   function handleRegister(e){
     e.preventDefault();
     
     setTarefas([...tarefas, input]);
     setInput(''); 
-    /*
-      Aula 31
-    alert("Usuário registrado com sucesso!");
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })*/
+    
   }
 
   return(
@@ -54,20 +40,7 @@ function App(){
           onChange={(e)=> setInput(e.target.value) }
         /> <br/>
 
-        {/*Aula 31
-        <label>Email:</label><br/>
-        <input 
-          placeholder='Digite seu email'
-          value={email}
-          onChange={(e)=> setEmail(e.target.value) }
-        /> <br/>
-
-        <label>Idade:</label><br/>
-        <input 
-          placeholder='Digite seu idade'
-          value={idade}
-          onChange={(e)=> setIdade(e.target.value) }
-        /> */} <br/>
+         <br/>
 
         <button type='submit'>Registrar</button>
 
@@ -75,31 +48,125 @@ function App(){
 
       <br/><br/>
 
-      {/*<div>*/}
       <ul>
-
         {tarefas.map(tarefa => (
           <li key={tarefa}>{tarefa}</li>
         ))}
-        {/*
-          Aula 31
-          <span>Bem vindo: {user.nome}</span> <br/>
-          <span>Idade: {user.idade}</span> <br/>
-          <span>Email: {user.email}</span> <br/>
-      */}
       </ul>
-      {/*</div>*/}
-            
-      {/*//Aula 30
-      <h1>Componente App</h1>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={ () =>handleChangeName('Luiz Fernando')}>
-        Mudar nome
-        </button>
-      <Nome aluno="Fernando Gonçalves" idade={30}/> */}
+
     </div>
   );
 }
 
 export default App;
 
+//------------- Aula 30 ------------------
+//import Nome from './components/Nome';
+
+//const [aluno, setAluno] = useState("Programador")
+
+//function handleChangeName(nome){
+//  setAluno(nome);
+//}
+
+/*
+<h1>Componente App</h1>
+<h2>Olá: {aluno}</h2>
+<button onClick={ () =>handleChangeName('Luiz Fernando')}>
+  Mudar nome
+  </button>
+<Nome aluno="Fernando Gonçalves" idade={30}/> */
+
+//------------- Aula 31 --------------------
+  //const [nome, setNome] = useState('');
+  //const [email, setEmail] = useState('');
+  //const [idade, setIdade] = useState(0);
+
+  /*const [user, setUser] = useState({
+    nome: 'Joaquim',
+    idade: '55',
+    email: 'fulano@fulano.com'
+  });*/
+
+/*
+  alert("Usuário registrado com sucesso!");
+  setUser({
+    nome: nome,
+    idade: idade,
+    email: email
+  })*/
+
+/*
+  <label>Email:</label><br/>
+  <input 
+    placeholder='Digite seu email'
+    value={email}
+    onChange={(e)=> setEmail(e.target.value) }
+  /> <br/>
+
+  <label>Idade:</label><br/>
+  <input 
+    placeholder='Digite seu idade'
+    value={idade}
+    onChange={(e)=> setIdade(e.target.value) }
+  /> */
+
+  /*<div>*/
+
+  /*
+    <span>Bem vindo: {user.nome}</span> <br/>
+    <span>Idade: {user.idade}</span> <br/>
+    <span>Email: {user.email}</span> <br/>
+*/
+/*</div>*/
+
+/* -------------- Aula 32 -----------------------
+import { useState, useEffect } from 'react';
+
+function App(){
+  
+
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar conta de luz',
+    'Estudar React JS', 
+    'Estudar Teclado' 
+  ]);
+
+  function handleRegister(e){
+    e.preventDefault();
+    
+    setTarefas([...tarefas, input]);
+    setInput(''); 
+    
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando Usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label><br/>
+        <input 
+          placeholder='Digite uma tarefa'
+          value={input}
+          onChange={(e)=> setInput(e.target.value) }
+        /> <br/>
+
+         <br/>
+
+        <button type='submit'>Registrar</button>
+
+      </form>
+
+      <br/><br/>
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}*/
