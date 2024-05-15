68db31239ca7055c48d7ae1d3000e1139fd0c9ba
Fernando Silvano Gonçalves <fernandosilgon@gmail.com>
2024-05-15 19:10:26 -0300

    Seção 5 aulas 42 - 45

--- secao5/prime/src/pages/Home/index.js
+++ secao5/prime/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import './home.css';
 
 // URL DA API https://api.themoviedb.org/3/movie/now_playing?api_key=05a0744bc4d02dc0be76ba4187f237f0&language=pt-br
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilmes(){
@@ -16,14 +19,36 @@ function Home(){
                 }
             })
 
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0,10))
+            setLoading(false);
         }
 
         loadFilmes();
     }, [])
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando Filmes...</h2>
+            </div>
+
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem Vindo A Home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                    <article key={filme.id} >
+                        <strong>{ filme.title }</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                    </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
